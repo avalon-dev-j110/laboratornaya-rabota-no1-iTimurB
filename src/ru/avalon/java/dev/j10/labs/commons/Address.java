850a2054a850ef996d2d49749bfcdfbc905994cc
Tim <Tim@Tim-PC>
2020-03-31 13:47:32 +0300

    Add get/set

--- src/ru/avalon/java/dev/j10/labs/commons/Address.java
+++ src/ru/avalon/java/dev/j10/labs/commons/Address.java
@@ -24,9 +24,28 @@ public class Address {
              this.street = street;
              this.number = number;
          }
-        
-         @Override
+
+    public String getTown() {
+        return town;
+    }
+    public void setTown(String town) {
+        this.town = town;
+    }
+    public String getStreet() {
+        return street;
+    }
+    public void setStreet(String street) {
+        this.street = street;
+    }
+    public int getNumber() {
+        return number;
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }        
+         
+    @Override
           public String toString (){
-             return "| Town - " + town + ", Street - " + street + ", Number - " + number;
+             return "| Town - " + getTown() + ", Street - " + getStreet() + ", Number - " + getNumber();
           }
 }
